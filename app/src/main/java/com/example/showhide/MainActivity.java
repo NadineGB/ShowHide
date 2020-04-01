4e0f3a6f157986aa4c7e3c319c99cce67b40ef60
Nadine Groot-Bernink <nadine.groot-bernink@mediabeam.com>
2020-04-01 11:44:35 +0200

    Lösung V3

--- app/src/main/java/com/example/showhide/MainActivity.java
+++ app/src/main/java/com/example/showhide/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.showhide;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +10,19 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     TextView txt;
+    SeekBar mySeekBar;
 
     public void show(View view) {
 
         txt.setVisibility(View.VISIBLE);
+        mySeekBar.setVisibility(View.VISIBLE);
 
     }
 
     public void hide(View view) {
 
         txt.setVisibility(View.INVISIBLE);
+        mySeekBar.setVisibility(View.INVISIBLE);
     }
 
     @Override
@@ -27,5 +31,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txt = findViewById(R.id.textView);
+        mySeekBar = findViewById(R.id.mySeekbar);
     }
 }
