a0d11d9d68b5d8031a6b7d0490cc8c4718092240
Nadine Groot-Bernink <nadine.groot-bernink@mediabeam.com>
2020-04-01 11:12:50 +0200

    Lösung V1

--- app/src/main/java/com/example/showhide/MainActivity.java
+++ app/src/main/java/com/example/showhide/MainActivity.java
@@ -1,10 +1,28 @@
 package com.example.showhide;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txt;
+    Button hide;
+    Button show;
+
+    public void show(View view) {
+        txt = findViewById(R.id.textView);
+        txt.setVisibility(View.VISIBLE);
+
+    }
+
+    public void hide(View view) {
+        txt = findViewById(R.id.textView);
+        txt.setVisibility(View.INVISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
